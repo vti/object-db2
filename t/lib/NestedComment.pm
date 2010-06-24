8e53d0f42f6df246c49747144bc0c4ed2fd7e5ea
vti <viacheslav.t@gmail.com>
2010-06-24 15:24:50 +0200

    Added nested comments example implementation

---
+++ t/lib/NestedComment.pm
@@ -0,0 +1,81 @@
+package NestedComment;
+
+use strict;
+use warnings;
+
+use base 'ObjectDB';
+
+__PACKAGE__->schema('nested_comments')
+  ->belongs_to(
+    parent => {foreign_class => 'NestedComment', map => {parent_id => 'id'}})
+  ->has_many(ansestors =>
+      {foreign_class => 'NestedComment', map => {id => 'parent_id'}})
+  ->proxy(master => {column => 'master_type'});
+
+sub create {
+    my $self = shift;
+    my $class = ref($self) ? ref($self) : $self;
+    $self = $class->new(@_) unless ref($self);
+
+    my $rgt           = 1;
+    my $level         = 0;
+    my $comment_count = 0;
+
+    my $conn = $self->conn;
+
+    if ($self->column('parent_id')) {
+        my $parent = $self->find_related('parent');
+
+        if ($parent) {
+            $self->column(master_id   => $parent->column('master_id'));
+            $self->column(master_type => $parent->column('master_type'));
+
+            $level = $parent->column('level') + 1;
+
+            $rgt = $parent->column('lft');
+        }
+    }
+
+    $comment_count = $class->count(
+        conn        => $conn,
+        master_type => $self->column('master_type'),
+        master_id   => $self->column('master_id')
+    );
+
+    if ($comment_count) {
+        my $left = $class->find(
+            conn  => $conn,
+            where => [
+                master_id   => $self->column('master_id'),
+                master_type => $self->column('master_type'),
+                parent_id   => $self->column('parent_id')
+            ],
+            order_by => 'addtime DESC, id DESC',
+            single   => 1
+        );
+
+        $rgt = $left->column('rgt') if $left;
+
+        $class->update(
+            conn  => $conn,
+            set   => [rgt => \'rgt + 2'],
+            where => [rgt => {'>' => $rgt}]
+        );
+
+        $class->update(
+            conn  => $conn,
+            set   => [lft => \'lft + 2'],
+            where => [lft => {'>' => $rgt}]
+        );
+    }
+
+    $self->column(lft   => $rgt + 1);
+    $self->column(rgt   => $rgt + 2);
+    $self->column(level => $level);
+
+    $self->column(addtime => time) unless $self->column('addtime');
+
+    return $self->SUPER::create;
+}
+
+1;
