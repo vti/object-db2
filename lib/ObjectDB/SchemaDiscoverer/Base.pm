ad64395ad57b227079038ef1c1481a829a178be1
vti <viacheslav.t@gmail.com>
2011-07-21 18:17:05 +0300

    Removed attributes

--- lib/ObjectDB/SchemaDiscoverer/Base.pm
+++ lib/ObjectDB/SchemaDiscoverer/Base.pm
@@ -5,9 +5,21 @@ use warnings;
 
 use base 'ObjectDB::Base';
 
-__PACKAGE__->attr([qw/table/]);
-__PACKAGE__->attr([qw/columns primary_key unique_keys/] => sub { [] });
-__PACKAGE__->attr('auto_increment');
+sub table { $_[0]->{table} }
+
+sub auto_increment {
+    @_ > 1 ? $_[0]->{auto_increment} = $_[1] : $_[0]->{auto_increment};
+}
+sub columns { @_ > 1 ? $_[0]->{columns} = $_[1] : $_[0]->{columns} }
+
+sub primary_key {
+    @_ > 1 ? $_[0]->{primary_key} = $_[1] : $_[0]->{primary_key}
+      || [];
+}
+
+sub unique_keys {
+    @_ > 1 ? $_[0]->{unique_keys} = $_[1] : $_[0]->{unique_keys} || [];
+}
 
 sub unquote {
     my $self  = shift;
