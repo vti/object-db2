0a293be3d7943f508915313a3e95b28447ba711f
yko <ykorshak@gmail.com>
2011-07-21 17:51:00 +0300

    removed attrs from SQL::Base

--- lib/ObjectDB/SQL/Base.pm
+++ lib/ObjectDB/SQL/Base.pm
@@ -7,9 +7,19 @@ use base 'ObjectDB::Base';
 
 use ObjectDB::SQL::Where;
 
-__PACKAGE__->attr(is_built => 0);
-__PACKAGE__->attr([qw/driver table order_by limit offset/]);
-__PACKAGE__->attr(['columns'] => sub { [] });
+sub BUILD {
+    my $self = shift;
+    $self->{is_built} = 0  if not exists $self->{is_build};
+    $self->{columns}  = [] if not exists $self->{columns};
+}
+
+sub is_built   { @_ > 1 ? $_[0]->{is_built} = $_[1] : $_[0]->{is_built} }
+sub driver   { @_ > 1 ? $_[0]->{driver} = $_[1] : $_[0]->{driver} }
+sub table    { @_ > 1 ? $_[0]->{table} = $_[1] : $_[0]->{table} }
+sub order_by { @_ > 1 ? $_[0]->{order_by} = $_[1] : $_[0]->{order_by} }
+sub limit    { @_ > 1 ? $_[0]->{limit} = $_[1] : $_[0]->{limit} }
+sub offset   { @_ > 1 ? $_[0]->{offset} = $_[1] : $_[0]->{offset} }
+sub columns  { @_ > 1 ? $_[0]->{columns} = $_[1] : $_[0]->{columns} }
 
 use overload '""' => sub { shift->to_string }, fallback => 1;
 use overload 'bool' => sub { shift; }, fallback => 1;
