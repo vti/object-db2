eb1b92c0e9b85cb7155da702beead458f95430d6
vti <viacheslav.t@gmail.com>
2011-10-26 20:47:43 +0300

    Cleanup

--- t/lib/TestSQLBase.pm
+++ t/lib/TestSQLBase.pm
@@ -6,7 +6,6 @@ use warnings;
 use base 'TestBase';
 
 use Scalar::Util qw(refaddr);
-use Sub::Override;
 
 use TestEnv;
 use TestDB;
@@ -17,15 +16,7 @@ sub startup : Test(startup) {
     my $self = shift;
 
     if ($self->driver ne TestDB->dbh->{Driver}->{Name}) {
-        no strict;
-        no warnings 'redefine';
-        #*{"Test::Class::_has_no_tests"} = sub {1};
-        #*{"Test::Class::_run_method"} = sub {1};
-        #*{"Test::Class::_total_num_tests"} = sub {0};
-
-        #*{ref($self) . "::_has_no_tests"} = sub {1};
-        #*{ref($self) . "::_run_method"} = sub {0};
-        #*{ref($self) . "::_total_num_tests"} = sub {0};
+        # TODO
     }
     else {
         $self->SUPER::startup(@_);
